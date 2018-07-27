6a5cb1a369b673355913a809353968add0b6d8e0
Philipp Moritz <pcmoritz@gmail.com>
2018-07-27 00:55:43 -0700

    fix

--- asv/environment.py
+++ asv/environment.py
@@ -654,6 +654,5 @@ class ExistingEnvironment(Environment):
 
     def run(self, args, **kwargs):
         log.debug("Running '{0}' in {1}".format(' '.join(args), self.name))
-        kwargs['capture_output'] = False
         return util.check_output([
-            self._executable] + args, **kwargs)
+            self._executable] + args, capture_output=False, **kwargs)
